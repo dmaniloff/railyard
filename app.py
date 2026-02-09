f685c71e63b7a85f1ea477df5ff125922eb1f3cf
Diego Maniloff <diego.maniloff@gmail.com>
2026-02-08 20:40:40 -0500

    basic auth.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 import gradio as gr
 
 from railyard.frontend import create_app
@@ -6,9 +8,13 @@ demo = create_app()
 
 
 if __name__ == "__main__":
+    auth_user = os.environ["RAILYARD_USER"]
+    auth_pass = os.environ["RAILYARD_PASS"]
+
     demo.launch(
         server_name="0.0.0.0",
         server_port=7860,
         share=False,
         theme=gr.themes.Soft(),
+        auth=(auth_user, auth_pass),
     )
